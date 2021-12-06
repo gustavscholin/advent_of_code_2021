9bf7b0e734dc2b2190e1c065f2eb37ef587ef8f3
Gustav Schölin <gustav@scholin.dev>
2021-12-06 18:18:06 +0100

    Add helper methods for reading input

---
+++ utils/read_input.py
@@ -0,0 +1,18 @@
+def read_input(path: str, type: str = "str"):
+    with open(path, "r") as f:
+        if type == "str":
+            return f.read().splitlines()
+        elif type == "int":
+            return [int(i) for i in f.read().splitlines()]
+        else:
+            raise Exception(f'Invalid type "{type}", should be "str" or "int"')
+
+
+def read_input_single_line(path: str, type: str = "str", sep: str = ","):
+    with open(path, "r") as f:
+        if type == "str":
+            return f.read().split(sep)
+        elif type == "int":
+            return [int(i) for i in f.read().split(sep)]
+        else:
+            raise Exception(f'Invalid type "{type}", should be "str" or "int"')
