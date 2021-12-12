d1e1c5ac4fc1d047a2a8e1534031e67c5aa53330
Gustav <gustav@scholin.dev>
2021-12-12 23:07:42 +0100

    Add method for reading input as matrix

--- utils/read_input.py
+++ utils/read_input.py
@@ -16,3 +16,13 @@ def read_input_single_line(path: str, type: str = "str", sep: str = ","):
             return [int(i) for i in f.read().split(sep)]
         else:
             raise Exception(f'Invalid type "{type}", should be "str" or "int"')
+
+
+def read_input_as_matrix(path: str, type: str = "str"):
+    with open(path, "r") as f:
+        if type == "str":
+            return [[i for i in row] for row in f.read().splitlines()]
+        elif type == "int":
+            return [[int(i) for i in row] for row in f.read().splitlines()]
+        else:
+            raise Exception(f'Invalid type "{type}", should be "str" or "int"')
