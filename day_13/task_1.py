e287f798005e8e9c54eb20cb0180bda67179b270
Gustav <gustav@scholin.dev>
2021-12-15 21:29:06 +0100

    Add solutions for day 13

---
+++ day_13/task_1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from copy import deepcopy
+
+
+def read_input(path: str):
+    with open(path, "r") as f:
+        return f.read().split("\n\n")
+
+
+if __name__ == "__main__":
+    coords, folds = read_input("day_13/input.txt")
+    fold = folds.splitlines()[0].strip("fold along ").split("=")
+    coords = [[int(i) for i in coord.split(",")] for coord in coords.splitlines()]
+
+    line = int(fold[1])
+    axis = fold[0]
+    new_coords = set()
+    dim = 0 if axis == "x" else 1
+    for coord in coords:
+        if coord[dim] <= line:
+            new_coords.add(tuple(coord))
+        elif coord[dim] > line:
+            new_coord = deepcopy(coord)
+            new_coord[dim] = line - (coord[dim] - line)
+            new_coords.add(tuple(new_coord))
+
+    print(len(new_coords))
---
+++ day_13/task_2.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from copy import deepcopy
+
+
+def read_input(path: str):
+    with open(path, "r") as f:
+        return f.read().split("\n\n")
+
+
+if __name__ == "__main__":
+    coords, folds = read_input("day_13/input.txt")
+    folds = [fold.strip("fold along ").split("=") for fold in folds.splitlines()]
+    coords = [[int(i) for i in coord.split(",")] for coord in coords.splitlines()]
+
+    for axis, line in folds:
+        line = int(line)
+        new_coords = []
+        dim = 0 if axis == "x" else 1
+        for coord in coords:
+            if coord[dim] <= line:
+                new_coords.append(coord)
+            elif coord[dim] > line:
+                new_coord = deepcopy(coord)
+                new_coord[dim] = line - (coord[dim] - line)
+                new_coords.append(new_coord)
+        coords = new_coords
+
+    x_max = max([coord[0] for coord in coords])
+    y_max = max([coord[1] for coord in coords])
+
+    for i in range(y_max + 1):
+        line = []
+        for j in range(x_max + 1):
+            if [j, i] in coords:
+                line.append("#")
+            else:
+                line.append(".")
+        print("".join(line))
