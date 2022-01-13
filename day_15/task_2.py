d7d70ae6a382f7130114e5422716c35131b85637
Gustav <gustav@scholin.dev>
2022-01-13 19:49:20 +0100

    Add complete solution for day 15 task 2

--- day_15/task_2.py
+++ day_15/task_2.py
@@ -34,21 +34,32 @@ def dijkstra(
 
 if __name__ == "__main__":
     risk_map = np.array(read_input_as_matrix("day_15/input.txt", "int"))
+
+    full_risk_map = np.vstack(
+        [np.hstack([risk_map + i + j for j in range(5)]) for i in range(5)]
+    )
+
+    mask = full_risk_map > 9
+    full_risk_map_mod = (full_risk_map % 10) + 1
+    full_risk_map[mask] = full_risk_map_mod[mask]
+
     vertices = []
     edges = {}
-    for i in range(len(risk_map)):
-        for j in range(len(risk_map[i])):
+    for i in range(len(full_risk_map)):
+        for j in range(len(full_risk_map[i])):
             vertice = (i, j)
             vertices.append(vertice)
             neighbour_edges = {}
             for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                 neighbour = np.add(vertice, offset)
-                if np.all(np.logical_and(neighbour >= 0, neighbour < len(risk_map))):
-                    neighbour_edges[tuple(neighbour)] = risk_map[neighbour[0]][
-                        neighbour[1]
+                if np.all(
+                    np.logical_and(neighbour >= 0, neighbour < len(full_risk_map))
+                ):
+                    neighbour_edges[tuple(neighbour)] = full_risk_map[
+                        neighbour[0], neighbour[1]
                     ]
             edges[vertice] = neighbour_edges
 
     start = (0, 0)
-    destination = (len(risk_map) - 1, len(risk_map) - 1)
+    destination = (len(full_risk_map) - 1, len(full_risk_map) - 1)
     print(dijkstra(vertices, edges, start, destination))
